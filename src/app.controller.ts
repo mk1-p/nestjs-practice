5286dd6e63f6d8e84d116774da2b5590695a3f6f
mk1-p <mk1.dev662@gmail.com>
2023-11-14 17:39:24 +0900

    CLN: 로직 제거 및 validator-pipe 설정 추가, 라이브러리 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('prefix') // Controller Class Prefix Path
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Post()
-  getPost() {
-    return 'get Post!';
-  }
 }
