abae1e0fcd3c851525beb16ca334e532404252ab
mk1-p <mk1.dev662@gmail.com>
2023-11-14 18:19:03 +0900

    CLN: Remove post list in controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,36 +1,6 @@
-import { Body, Controller, Delete, Get, Header, NotFoundException, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Header, Param, Post, Put } from "@nestjs/common";
 import { PostsService } from './posts.service';
 import { PostModelDto } from './post-model.dto';
-import { Builder } from 'builder-pattern';
-
-let posts: PostModelDto[];
-posts = [
-  // builder-pattern 적용 해보기
-  Builder(PostModelDto)
-    .id(1)
-    .author("newjeans_official")
-    .title("뉴진스 민지")
-    .content("메이크업 고치고 있는 민지")
-    .likeCount(1000)
-    .commentCount(999999)
-    .build(),
-  Builder(PostModelDto)
-    .id(2)
-    .author("newjeans_official")
-    .title("뉴진스 혜린")
-    .content("노래 연습하고 있는 혜린")
-    .likeCount(1000)
-    .commentCount(999999)
-    .build(),
-  Builder(PostModelDto)
-    .id(3)
-    .author("blackpink_official")
-    .title("블랙핑크 로제")
-    .content("공연 연습하고 있는 로제")
-    .likeCount(1000)
-    .commentCount(999999)
-    .build(),
-];
 
 @Controller('posts')
 export class PostsController {
